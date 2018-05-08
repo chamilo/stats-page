dfbb2ddf19f7d66a7b19f72da684d45de399cbcd
Yannick Warnier <ywarnier@beeznest.org>
2018-05-08 11:45:39 -0500

    Add users count as a web service

---
+++ count-users.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This script shows the total number of users of Chamilo, globally
+ * http://version.chamilo.org/stats/
+ * @package chamilo.website.stats
+ * @author Yannick Warnier <yannick@chamilo.org>
+ */
+/**
+ * Includes the database and versions settings
+ */
+include_once('connection.php');
+$dsn = 'mysql:dbname='.DEFDB.';host='.SERVER;
+try {
+    $db = new PDO($dsn, DBUSER, DBPASSWORD);
+} catch (PDOException $e) {
+    die('Could not connect to the database');
+}
+$totalUsers = 0;
+$sqlb = "SELECT id, max(updated_on), number_of_users 
+    FROM community
+    WHERE portal_url NOT LIKE '%127.0.%' 
+    AND portal_url NOT LIKE '%192.168.%' 
+    AND portal_url NOT LIKE '%localhost%'
+    AND number_of_users > 65
+    GROUP BY portal_url 
+    ";
+$resb = $db->query($sqlb);
+
+if ($resb->rowCount() > 0) {
+    while ($row = $resb->fetch(PDO::FETCH_ASSOC)) {
+        $total_users += $row['number_of_users'];
+    }
+}
+echo $total_users;
